5804c05f19acbe9edbf32ca949b8495d758a8f81
Mahadev Dhobale <88610696+Mahadhav1999@users.noreply.github.com>
2025-08-20 00:57:37 +0530

    Create hello.java

---
+++ hello.java
@@ -0,0 +1,6 @@
+public class Main {
+  public static void main(String[] args){
+    System.out.println("Hello from java!");
+    System.out.println("Hello from batch-40 learning CI With Jenkins!");
+  }
+}
