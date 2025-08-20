7f151fea6eb7df063ece90828758f437506d5c8b
Mahadev Dhobale <88610696+Mahadhav1999@users.noreply.github.com>
2025-08-20 14:40:42 +0530

    Update hello.java

--- hello.java
+++ hello.java
@@ -1,6 +1,6 @@
 class Main {
   public static void main(String[] args){
     System.out.println("Hello from java:)");
-    System.out.println("Welcome to batch-40 learning CI With Jenkins!");
+    System.out.println("Welcome to batch-40 of DevOps learning CI With Jenkins!");
   }
 }
